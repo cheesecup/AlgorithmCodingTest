6bbb30cac2a89c5880f231e13b5be9a3d52bd7b3
cheesecup <kdy8252@naver.com>
2022-04-19 22:21:24 +0900

    001풀이 추가

---
+++ src/Chapter_03/Ex_002.java
@@ -0,0 +1,9 @@
+package Chapter_03;
+
+public class Ex_002 {
+
+    /* 평균 구하기 */
+    public void P1546() {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Chapter_01.Ex_000;
+import Chapter_03.Ex_001;
+
+public class Main {
+    public static void main(String[] args) {
+        Ex_000 ex_000 = new Ex_000();
+        Ex_001 ex_001 = new Ex_001();
+
+    }
+}
