fa09e7c49f447c1a506ace2cf7000d2a349e37db
cheesecup <kdy8252@naver.com>
2022-04-21 00:16:14 +0900

    Ex_003 추가

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 import Chapter_01.Ex_000;
 import Chapter_03.Ex_001;
 import Chapter_03.Ex_002;
+import Chapter_03.Ex_003;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Ex_000 ex_000 = new Ex_000();
         Ex_001 ex_001 = new Ex_001(); // 숫자의 합 구하기
-        Ex_002 ex_002 = new Ex_002();
-        ex_002.P1546();
+        Ex_002 ex_002 = new Ex_002(); // 평균 구하기
+        Ex_003 ex_003 = new Ex_003(); // 구간 합 구하기
+        ex_003.P11659();
 
     }
 }
