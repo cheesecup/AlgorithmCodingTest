f0bfa91f14aa1ffc9cb1dbce08d013cb5689f6e2
cheesecup <kdy8252@naver.com>
2022-04-20 23:45:22 +0900

    Ex_002 추가

---
+++ src/Chapter_03/Ex_003.java
@@ -0,0 +1,9 @@
+package Chapter_03;
+
+public class Ex_003 {
+
+    /* 구간 합 구하기 */
+    public void P11659() {
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import Chapter_01.Ex_000;
 import Chapter_03.Ex_001;
+import Chapter_03.Ex_002;
 
 public class Main {
     public static void main(String[] args) {
         Ex_000 ex_000 = new Ex_000();
-        Ex_001 ex_001 = new Ex_001();
+        Ex_001 ex_001 = new Ex_001(); // 숫자의 합 구하기
+        Ex_002 ex_002 = new Ex_002();
+        ex_002.P1546();
 
     }
 }
