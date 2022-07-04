6383e3f7d19e6ad621bc72040cc9ea9ff238916b
cheesecup <kdy8252@naver.com>
2022-07-05 00:35:47 +0900

    예제 9번 추가

--- src/main/CodingTest/Chapter_03/Ex_009.java
+++ src/main/CodingTest/Chapter_03/Ex_009.java
@@ -8,39 +8,122 @@ import java.util.StringTokenizer;
 
 /* 슬라이딩 윈도우 - DNA 비밀번호*/
 public class Ex_009 {
+    static int[] myArr;
+    static int[] checkArr; // 비밀번호 체크 배열
+    static int checkSecret;
+
     public void P12891() throws IOException {
-        // {‘A’, ‘C’, ‘G’, ‘T’} == {65, 67, 71, 84}
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        int S = Integer.parseInt(st.nextToken()); // DNA 문자열 길이 S
-        int P = Integer.parseInt(st.nextToken()); // 부분 문자열 길이 P
+        int Result = 0;
+        checkArr = new int[4];
+        myArr = new int[4];
+        checkSecret = 0;
+        int S = Integer.parseInt(st.nextToken()); // 문자열 크기 S
+        int P = Integer.parseInt(st.nextToken()); // 부분 문자열 크기 P
 
+        char[] A = new char[S]; // 문자열 데이터
         st = new StringTokenizer(bf.readLine());
-        String dna = st.nextToken();
-        int[] arr = dnaSub(dna, S);
-        System.out.println(Arrays.toString(arr));
+//        A = st.nextToken().toCharArray();
+        String inputDNA = st.nextToken();
+        for (int i=0; i<S; i++) {
+            A[i] = inputDNA.charAt(i);
+        }
+        System.out.println("A[]=" + Arrays.toString(A));
 
-        int[] checkArr = new int[4];
+        /* {‘A’, ‘C’, ‘G’, ‘T’} */
         st = new StringTokenizer(bf.readLine());
         for (int i=0; i<4; i++) {
             checkArr[i] = Integer.parseInt(st.nextToken());
+            if (checkArr[i] == 0) {
+                checkSecret++;
+            }
+        }
+        System.out.println("checkArr[]=" + Arrays.toString(checkArr));
+
+        for (int i=0; i<P; i++) {
+            add(A[i]);
+        }
+        if (checkSecret == 4) {
+            Result++;
         }
 
-
-
-    }
-
-    // 입력 문자열 추출
-    public static int[] dnaSub(String dna, int len) {
-        int[] arr = new int[26];
-        for (int i=0; i<len; i++) {
-            int res = dna.charAt(i) - 65;
-            arr[res]++;
+        // 슬라이딩 윈도우 처리
+        for (int i=P; i<S; i++) {
+            int j = i - P;
+            add(A[i]);
+            remove(A[j]);
+            if (checkSecret == 4) {
+                Result++;
+            }
         }
-        return arr;
+        System.out.println(Result);
+        bf.close();
     }
 
-    // 부분 문자열 추출
+    // 문자 더하기 함수
+    private static void add(char c) {
+        switch (c){
+            case 'A':
+                myArr[0]++;
+                if (myArr[0] == checkArr[0]) {
+                    checkSecret++;
+                }
+                break;
 
-    //
+            case 'C':
+                myArr[1]++;
+                if (myArr[1] == checkArr[1]) {
+                    checkSecret++;
+                }
+                break;
+
+            case 'G':
+                myArr[2]++;
+                if (myArr[2] == checkArr[2]) {
+                    checkSecret++;
+                }
+                break;
+
+            case 'T':
+                myArr[3]++;
+                if (myArr[3] == checkArr[3]) {
+                    checkSecret++;
+                }
+                break;
+        }
+    }
+
+    // 문자 빼기 함수
+    public static void remove(char c) {
+        switch (c) {
+            case 'A':
+                if (myArr[0] == checkArr[0]) {
+                    checkSecret--;
+                }
+                myArr[0]--;
+                break;
+
+            case 'C':
+                if (myArr[1] == checkArr[1]) {
+                    checkSecret--;
+                }
+                myArr[1]--;
+                break;
+
+            case 'G':
+                if (myArr[2] == checkArr[2]) {
+                    checkSecret--;
+                }
+                myArr[2]--;
+                break;
+
+            case 'T':
+                if (myArr[3] == checkArr[3]) {
+                    checkSecret--;
+                }
+                myArr[3]--;
+                break;
+        }
+    }
 }
